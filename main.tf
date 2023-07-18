8add93ffbd3b3ee2450c5b64975b21a9f9d86d59
Silpashree Acharya <silpashree@bootlabstech.com>
2023-07-18 14:25:14 +0530

    changed bucket id default

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "google_logging_project_bucket_config" "basic" {
     project    = var.project_id
     location  = "global"
     retention_days = var.retention_days
-    bucket_id = "_Default"
+    bucket_id = "custom-bucket"
 }
