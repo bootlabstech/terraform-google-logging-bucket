1203842c221cbd3fa2f4d689ff7d1305b7f153ea
Silpashree Acharya <silpashree@bootlabstech.com>
2023-07-19 17:48:30 +0530

    changed location

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_logging_project_bucket_config" "basic" {
     project    = var.project_id
-    location  = "global"
+    location  = "asia"
     retention_days = var.retention_days
     bucket_id = "_Default"
 }
