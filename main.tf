1964d1baafb2a907cae585c7b116dfa79d3c9049
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-21 12:05:47 +0530

    latest provider

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_logging_project_bucket_config" "basic" {
-    project    = var.project_id
-    location  = "asia-south1"
-    retention_days = var.retention_days
-    bucket_id = "logging_bucket"
+  project        = var.project_id
+  location       = "asia-south1"
+  retention_days = var.retention_days
+  bucket_id      = "logging_bucket"
 }
